a6d2e1585ae161b5e5fd7084f1af48dd5a83a794
caoxinyu <caoxinyu@xiaomi.com>
2025-04-18 20:17:40 +0800

    表达式

--- demo/src/type_cast.rs
+++ demo/src/type_cast.rs
@@ -218,3 +218,27 @@ fn main18() {
     let sum = parsed + turbo_parsed;
     println!{"Sum: {:?}", sum};
 }
+
+
+#[test]
+fn main19() {
+    let x = 5u32;
+
+    let y = {
+        let x_squared = x * x;
+        let x_cube = x_squared * x;
+
+        // 将此表达式赋给 `y`
+        x_cube + x_squared + x
+    };
+
+    let z = {
+        // 分号结束了这个表达式，于是将 `()` 赋给 `z`
+        2 * x;
+    };
+
+    println!("x is {:?}", x);
+    println!("y is {:?}", y);
+    println!("z is {:?}", z);
+}
+
