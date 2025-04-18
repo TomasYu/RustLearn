44193f9c0aeab552dad8c37ce94675ba1787537b
caoxinyu <caoxinyu@xiaomi.com>
2025-04-18 16:32:16 +0800

    格式化

--- demo/src/formate_test.rs
+++ demo/src/formate_test.rs
@@ -115,7 +115,7 @@ fn main6() {
 
 
 
-use std::fmt; // 导入 `fmt` 模块。
+use std::fmt::{self, Display, Formatter}; // 导入 `fmt` 模块。
 
 // 定义一个包含单个 `Vec` 的结构体 `List`。
 struct List(Vec<i32>);
@@ -144,4 +144,55 @@ impl fmt::Display for List {
 fn main7() {
     let v = List(vec![1, 2, 3]);
     println!("{}", v);
+}
+
+
+
+
+
+struct City {
+    name: &'static str,
+    // 纬度
+    lat: f32,
+    // 经度
+    lon: f32,
+}
+
+impl Display for City {
+    // `f` 是一个缓冲区（buffer），此方法必须将格式化后的字符串写入其中
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        // `write!` 和 `format!` 类似，但它会将格式化后的字符串写入
+        // 一个缓冲区（即第一个参数f）中。
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+#[test]
+fn main8() {
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // 在添加了针对 fmt::Display 的实现后，请改用 {} 检验效果。
+        println!("{:?}", *color)
+    }
 }
