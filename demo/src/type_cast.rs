68313c5e19e80863de467ba1da2a1aa62f831d57
caoxinyu <caoxinyu@xiaomi.com>
2025-04-18 20:13:54 +0800

    字符串转换完成

--- demo/src/type_cast.rs
+++ demo/src/type_cast.rs
@@ -109,3 +109,112 @@ fn main11() {
              nanoseconds + inches);
 }
 
+
+
+
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+#[test]
+fn main12() {
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+}
+#[test]
+fn main13() {
+    let int = 5;
+    // 试试删除类型说明
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+}
+
+
+
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
+#[derive(Debug, PartialEq)]
+struct EvenNumber(i32);
+
+impl TryFrom<i32> for EvenNumber {
+    type Error = ();
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        if value % 2 == 0 {
+            Ok(EvenNumber(value))
+        } else {
+            Err(())
+        }
+    }
+}
+
+#[test]
+fn main14() {
+    // TryFrom
+
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+
+    // TryInto
+
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
+}
+
+
+// use std::fmt;
+
+// struct Circle {
+//     radius: i32
+// }
+
+// impl fmt::Display for Circle {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "Circle of radius {}", self.radius)
+//     }
+// }
+// #[test]
+// fn main16() {
+//     let circle = Circle { radius: 6 };
+//     println!("{}", circle.to_string());
+// }
+
+
+use std::string::ToString;
+
+struct Circle {
+    radius: i32
+}
+
+impl ToString for Circle {
+    fn to_string(&self) -> String {
+        format!("Circle of radius {:?}", self.radius)
+    }
+}
+
+#[test]
+fn main17() {
+    let circle = Circle { radius: 6 };
+    println!("{}", circle.to_string());
+}
+
+#[test]
+fn main18() {
+    let parsed: i32 = "5".parse().unwrap();
+    let turbo_parsed = "10".parse::<i32>().unwrap();
+
+    let sum = parsed + turbo_parsed;
+    println!{"Sum: {:?}", sum};
+}
